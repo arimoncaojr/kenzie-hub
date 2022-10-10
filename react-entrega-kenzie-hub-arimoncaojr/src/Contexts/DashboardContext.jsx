2c3ea3598d7ad0e129436ee729e4212208e0dbe4
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2022-10-10 18:31:30 -0300

    feat: alterações concluídas

---
+++ react-entrega-kenzie-hub-arimoncaojr/src/Contexts/DashboardContext.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState, createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Api } from "../services/api";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object({
+  title: yup.string().required("Nome não preenchido"),
+  status: yup.string().required("Status não selecionado"),
+});
+
+export const DashboardContext = createContext();
+
+export const DashboardProvider = ({ children }) => {
+  const [userInfo, setUserInfo] = useState([]);
+  const token = localStorage.getItem("@kenzieHub:Token");
+  const navigate = useNavigate();
+  const [modal, showModal] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  useEffect(() => {
+    Api.get("/profile", { headers: { Authorization: `Bearer ${token}` } })
+      .then((res) => setUserInfo(res.data))
+      .catch((err) => {
+        console.log(err);
+        window.localStorage.clear();
+        navigate("/login");
+      });
+  }, [token, navigate, userInfo.techs]);
+
+  function deleteTech(id) {
+    Api.delete(`/users/techs/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    }).then(() => toast.success("Tecnologia deletada com sucesso!"));
+  }
+
+  const submitTechInfo = (infos) => {
+    Api.post(
+      "/users/techs",
+      { ...infos },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
+      .then((res) => {
+        toast.success("Tecnologia criada com sucesso!");
+        showModal(false);
+        reset();
+      })
+      .catch((err) => {
+        err && toast.error("Tecnologia com mesmo nome já criada!");
+      });
+  };
+
+  return (
+    <DashboardContext.Provider
+      value={{
+        userInfo,
+        token,
+        deleteTech,
+        modal,
+        showModal,
+        submitTechInfo,
+        register,
+        handleSubmit,
+        errors,
+        reset,
+      }}
+    >
+      {children}
+    </DashboardContext.Provider>
+  );
+};
--- react-entrega-kenzie-hub-arimoncaojr/src/pages/Dashboard/styles.jsx
+++ react-entrega-kenzie-hub-arimoncaojr/src/pages/Dashboard/styles.jsx
@@ -1,13 +1,24 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { TrashFill } from "@styled-icons/bootstrap/TrashFill";
+
+export const IconTrash = styled(TrashFill)`
+  color: var(--grey0);
+  width: 0.823rem;
+  height: 0.75rem;
+  cursor: pointer;
+  :hover {
+    color: var(--grey1);
+  }
+`;
 
 export const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-color: var(--grey4);
+  position: relative;
 
   header {
     display: flex;
@@ -35,12 +46,6 @@ export const Container = styled.div`
     justify-content: center;
     width: 100%;
     border-bottom: 1px solid var(--grey2);
-  }
-
-  h2 {
-    color: var(--grey0);
-    font-size: var(--font-title1);
-    font-weight: var(--font-weight1);
   }
 `;
 
@@ -88,7 +93,188 @@ export const DivInfos = styled.div`
   gap: 1rem;
   width: 80%;
 
-  p {
-    color: var(--grey0);
+  .div-btn-tech {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button {
+      background-color: var(--grey3);
+      border: none;
+      border-radius: var(--border-radius-default);
+      color: var(--grey0);
+      font-size: var(--font-title1);
+      width: 2.031rem;
+      height: 2rem;
+
+      :hover {
+        background-color: var(--grey2);
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    width: 100%;
+    background-color: var(--grey3);
+    border-radius: var(--border-radius-default);
+    padding-bottom: 20px;
+    padding-top: 20px;
+    li {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 95%;
+      height: 3.063rem;
+      background-color: var(--grey4);
+      border-radius: var(--border-radius-default);
+      margin-top: 6px;
+      cursor: pointer;
+
+      :hover {
+        background-color: var(--grey2);
+      }
+
+      h3 {
+        font-size: var(--font-title3);
+        font-weight: var(--font-weight1);
+        color: var(--grey0);
+        margin-left: 1rem;
+      }
+      div {
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+        margin-right: 1rem;
+      }
+      p {
+        font-size: var(--font-text3);
+        color: var(--grey1);
+      }
+    }
+  }
+`;
+
+export const P = styled.p`
+  color: var(--grey0);
+`;
+
+export const H2 = styled.h2`
+  color: var(--grey0);
+  font-size: var(--font-title1);
+  font-weight: var(--font-weight1);
+`;
+
+export const H3 = styled.h3`
+  color: var(--grey0);
+  font-size: var(--font-title3);
+  font-weight: var(--font-weight2);
+`;
+
+export const ContainerModal = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  min-height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  .modal {
+    display: flex;
+    flex-direction: column;
+    width: 23.063rem;
+    height: 21.375rem;
+    background-color: var(--grey3);
+    border-radius: 4px 4px 0px 0px;
+
+    .closeDiv {
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      gap: 10rem;
+      height: 3.125rem;
+      background-color: var(--grey2);
+      border-radius: 4px 4px 0px 0px;
+
+      h4 {
+        font-size: var(--font-text3);
+        color: var(--grey0);
+        font-weight: var(--font-weight1);
+      }
+
+      button {
+        border: transparent;
+        background-color: transparent;
+        color: var(--grey1);
+        font-size: 1rem;
+        font-weight: var(--font-weight2);
+
+        :hover {
+          color: var(--grey0);
+        }
+      }
+    }
+
+    form {
+      display: flex;
+      margin: 0 auto;
+      flex-direction: column;
+      gap: 1rem;
+      margin-top: 1rem;
+
+      button {
+        width: var(--btn-login-width);
+        height: var(--btn-login-height);
+        color: var(--grey0);
+        border: transparent;
+        border-radius: var(--border-radius-default);
+        background-color: var(--color-primary);
+        max-width: 100%;
+
+        :hover {
+          background-color: var(--color-primary-focus);
+        }
+      }
+    }
+  }
+`;
+
+export const Label = styled.label`
+  font-size: var(--font-text2);
+  color: ${(props) => props.colorText};
+`;
+
+export const Input = styled.input`
+  width: var(--input-login-width);
+  height: var(--input-login-height);
+  border: 1px solid ${(props) => props.borderColor};
+  background-color: var(--grey2);
+  border-radius: var(--border-radius-default);
+  color: var(--grey0);
+  outline: none;
+  max-width: 100%;
+
+  :focus {
+    border: 1px solid ${(props) => props.borderFocus};
+  }
+`;
+
+export const Select = styled.select`
+  width: var(--input-login-width);
+  height: var(--input-login-height);
+  border: 1px solid ${(props) => props.borderColor};
+  background-color: var(--grey2);
+  border-radius: var(--border-radius-default);
+  color: var(--grey0);
+  outline: none;
+  max-width: 100%;
+
+  :focus {
+    border: 1px solid ${(props) => props.borderFocus};
   }
 `;
